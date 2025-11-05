d6a6fe7734fad41ae96a09a05aab49068cc53100
Lars Vilhuber <lars.vilhuber@cornell.edu>
2025-11-04 21:54:30 -0500

    Updating build environment, fixing jupyter-book to v1

--- _setup.sh
+++ _setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This will initialize the Python environment needed for testing.
-[[ -z $1 ]] && VERSION=3.10 || VERSION=$1
+[[ -z $1 ]] && VERSION=3.12 || VERSION=$1
 python$VERSION -m venv venv-$VERSION
 source venv-$VERSION/bin/activate
 pip$VERSION install -r requirements.txt
